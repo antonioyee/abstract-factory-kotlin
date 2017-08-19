1e7123042c093bda5f0615c6e0c26e6e8d6c639f
Antonio Yee <yee.antonio@gmail.com>
2017-08-19 09:50:50 -0600

    cheese

---
+++ src/products/MozzarellaCheese.kt
@@ -0,0 +1,15 @@
+package products
+
+import `factory-pizzas`.Cheese
+
+/**
+ * Created by Antonio Yee <yee.antonio@gmail.com> on 19/08/17.
+ */
+
+class MozzarellaCheese : Cheese {
+
+	override fun toString(): String {
+		return "Shredded Mozzarella"
+	}
+
+}
---
+++ src/products/ReggianoCheese.kt
@@ -0,0 +1,15 @@
+package products
+
+import `factory-pizzas`.Cheese
+
+/**
+ * Created by Antonio Yee <yee.antonio@gmail.com> on 19/08/17.
+ */
+
+class ReggianoCheese : Cheese {
+
+	override fun toString(): String {
+		return "Reggiano Cheese"
+	}
+
+}
