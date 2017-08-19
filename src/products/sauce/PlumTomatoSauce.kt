2d4c478f65aeb4a069303e00a99cd18e839a352e
Antonio Yee <yee.antonio@gmail.com>
2017-08-19 10:41:36 -0600

    fixed

--- src/products/sauce/PlumTomatoSauce.kt
+++ src/products/sauce/PlumTomatoSauce.kt
@@ -6,7 +6,7 @@ import `factory-pizzas`.Sauce
  * Created by Antonio Yee <yee.antonio@gmail.com> on 19/08/17.
  */
 
-class PlumTonatoSauce : Sauce {
+class PlumTomatoSauce : Sauce {
 
 	override fun toString(): String {
 		return "Tomato sauce with plum tomatoes"
