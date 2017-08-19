77cc902a969238336b17fcedf1a8e3ddc2808a98
Antonio Yee <yee.antonio@gmail.com>
2017-08-19 09:45:29 -0600

    masa

---
+++ src/products/ThickCrustDough.kt
@@ -0,0 +1,15 @@
+package products
+
+import `factory-pizzas`.Dough
+
+/**
+ * Created by Antonio Yee <yee.antonio@gmail.com> on 19/08/17.
+ */
+
+class ThickCrustDough : Dough {
+
+	override fun toString(): String {
+		return "ThickCrust style extra thick crust dough"
+	}
+
+}
---
+++ src/products/ThinCrustDough.kt
@@ -0,0 +1,15 @@
+package products
+
+import `factory-pizzas`.Dough
+
+/**
+ * Created by Antonio Yee <yee.antonio@gmail.com> on 19/08/17.
+ */
+
+class ThinCrustDough : Dough {
+
+	override fun toString(): String {
+		return "Thin Crust Dough"
+	}
+
+}
