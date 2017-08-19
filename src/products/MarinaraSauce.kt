a52f4cbe8dd5d76afb70824ee240acde94320696
Antonio Yee <yee.antonio@gmail.com>
2017-08-19 09:47:45 -0600

    sauce

---
+++ src/products/MarinaraSauce.kt
@@ -0,0 +1,15 @@
+package products
+
+import `factory-pizzas`.Sauce
+
+/**
+ * Created by Antonio Yee <yee.antonio@gmail.com> on 19/08/17.
+ */
+
+class MarinaraSauce : Sauce {
+
+	override fun toString(): String {
+		return "Marinara Sauce"
+	}
+
+}
---
+++ src/products/PlumTonatoSauce.kt
@@ -0,0 +1,15 @@
+package products
+
+import `factory-pizzas`.Sauce
+
+/**
+ * Created by Antonio Yee <yee.antonio@gmail.com> on 19/08/17.
+ */
+
+class PlumTonatoSauce : Sauce {
+
+	override fun toString(): String {
+		return "Tomato sauce with plum tomatoes"
+	}
+
+}
