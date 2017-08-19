d0c21630fb27f832eb12ce0d584ee74994374701
Antonio Yee <yee.antonio@gmail.com>
2017-08-19 10:41:55 -0600

    sliced pepperoni

---
+++ src/products/pepperoni/SlicedPepperoni.kt
@@ -0,0 +1,15 @@
+package products.pepperoni
+
+import `factory-pizzas`.Pepperoni
+
+/**
+ * Created by Antonio Yee <yee.antonio@gmail.com> on 19/08/17.
+ */
+
+class SlicedPepperoni : Pepperoni {
+
+	override fun toString(): String {
+		return "Sliced Pepperoni"
+	}
+
+}
