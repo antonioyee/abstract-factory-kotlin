775bfeac1c972bfe328ee7d9b786b5827751b612
Antonio Yee <yee.antonio@gmail.com>
2017-08-19 10:17:35 -0600

    veggies

---
+++ src/products/veggies/Eggplant.kt
@@ -0,0 +1,15 @@
+package products.veggies
+
+import `factory-pizzas`.Veggies
+
+/**
+ * Created by Antonio Yee <yee.antonio@gmail.com> on 19/08/17.
+ */
+
+class Eggplant : Veggies {
+
+	override fun toString(): String {
+		return "Eggplant"
+	}
+
+}
---
+++ src/products/veggies/Garlic.kt
@@ -0,0 +1,15 @@
+package products.veggies
+
+import `factory-pizzas`.Veggies
+
+/**
+ * Created by Antonio Yee <yee.antonio@gmail.com> on 19/08/17.
+ */
+
+class Garlic : Veggies {
+
+	override fun toString(): String {
+		return "Garlic"
+	}
+
+}
