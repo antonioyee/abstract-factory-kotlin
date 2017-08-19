76d130faedd1962ae0e1bab4de70695ec9623078
Antonio Yee <yee.antonio@gmail.com>
2017-08-19 10:00:22 -0600

    clams

---
+++ src/products/FreshClams.kt
@@ -0,0 +1,15 @@
+package products
+
+import `factory-pizzas`.Clams
+
+/**
+ * Created by Antonio Yee <yee.antonio@gmail.com> on 19/08/17.
+ */
+
+class FreshClams : Clams {
+
+	override fun toString(): String {
+		return "Fresh clams from long island sound"
+	}
+
+}
---
+++ src/products/FrozenClams.kt
@@ -0,0 +1,15 @@
+package products
+
+import `factory-pizzas`.Clams
+
+/**
+ * Created by Antonio Yee <yee.antonio@gmail.com> on 19/08/17.
+ */
+
+class FrozenClams : Clams {
+
+	override fun toString(): String {
+		return "Frozen clams from chesapeake bay"
+	}
+
+}
