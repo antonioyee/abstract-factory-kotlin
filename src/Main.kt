7d953802adde77d379a5c95daf78652b50efc050
Antonio Yee <yee.antonio@gmail.com>
2017-08-20 19:00:32 -0600

    implementation

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,44 @@
+import pizzas.Pizza
+import store.ChicagoPizzaStore
+import store.NYPizzaStore
+import store.PizzaStore
+
 /**
  * Created by Antonio Yee <yee.antonio@gmail.com> on 19/08/17.
  */
-fun main(args: Array<String>) {
 
+fun main(args: Array<String>) {
+	val nyStore: PizzaStore = NYPizzaStore()
+	val chicagoStore: PizzaStore = ChicagoPizzaStore()
+
+	var pizza: Pizza? = nyStore.orderPizza("cheese")
+	println("Ethan ordered a ${pizza?.name}")
+	println("\n")
+
+	pizza = chicagoStore.orderPizza("cheese")
+	println("Joel ordered a ${pizza?.name}")
+	println("\n")
+
+	pizza = nyStore.orderPizza("clam")
+	println("Ethan ordered a ${pizza?.name}")
+	println("\n")
+
+	pizza = chicagoStore.orderPizza("clam")
+	println("Joel ordered a ${pizza?.name}")
+	println("\n")
+
+	pizza = nyStore.orderPizza("pepperoni")
+	println("Ethan ordered a ${pizza?.name}")
+	println("\n")
+
+	pizza = chicagoStore.orderPizza("pepperoni")
+	println("Joel ordered a ${pizza?.name}")
+	println("\n")
+
+	pizza = nyStore.orderPizza("veggie")
+	println("Ethan ordered a ${pizza?.name}")
+	println("\n")
+
+	pizza = chicagoStore.orderPizza("veggie")
+	println("Joel ordered a ${pizza?.name}")
 }
